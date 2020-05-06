f127899e9e36c1acb0b2ba29f2525fbc318369f2
Shoham Hoffman Haik <shoham9hoffman@gmail.com>
2020-05-06 15:07:52 +0300

    Api Improvement 3.0

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,39 @@
+using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            return Ok(new ApiResponse(404));
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
